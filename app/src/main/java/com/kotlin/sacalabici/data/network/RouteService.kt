694013c8614a36b1aa3eaf8515e555fec0a8dd71
alexmushi <A01705550@tec.mx>
2024-09-18 16:19:07 -0600

    Docs: Agregar comentarios a funciones y definiciones

--- app/src/main/java/com/kotlin/sacalabici/data/network/RouteService.kt
+++ app/src/main/java/com/kotlin/sacalabici/data/network/RouteService.kt
@@ -1,5 +1,6 @@
 package com.kotlin.sacalabici.data.network
 
+// Importaciones para funciones de red y JSON
 import android.util.Log
 import com.mapbox.geojson.Point
 import kotlinx.coroutines.Dispatchers
@@ -9,17 +10,29 @@ import org.json.JSONObject
 import java.net.HttpURLConnection
 import java.net.URL
 
+// Objeto que se encarga de enviar la ruta al servidor
 object RouteService {
 
-    suspend fun sendRoute(titulo: String, distancia: String, tiempo: String, nivel: String, start: Point, stopover: Point, end: Point
-    ): Boolean = withContext(Dispatchers.IO) {
+    // Función que envía la ruta al servidor
+    // Recibe los datos de la ruta y los puntos de inicio, descanso y final
+    suspend fun sendRoute(
+        titulo: String,
+        distancia: String,
+        tiempo: String,
+        nivel: String,
+        start: Point,
+        stopover: Point,
+        end: Point
+    ): Boolean = withContext(Dispatchers.IO) {   // Ejecuta la lógica con corrutinas
         try {
+            // URL de la solicitud POST para registrar la ruta
             val url = URL("http://3.145.117.182:8080/mapa/registrarRuta")
             val connection = url.openConnection() as HttpURLConnection
             connection.requestMethod = "POST"
             connection.setRequestProperty("Content-Type", "application/json")
-            connection.doOutput = true
+            connection.doOutput = true // Indica que se enviarán datos en la solicitud
 
+            // Se crea un arreglo JSON con los puntos de inicio, descanso y final
             val coordinatesArray = JSONArray().apply {
                 put(JSONObject().apply {
                     put("latitud", start.latitude())
@@ -38,6 +51,7 @@ object RouteService {
                 })
             }
 
+            // Crea el objeto JSON con la información de la ruta a enviar
             val jsonInputString = JSONObject().apply {
                 put("titulo", titulo)
                 put("distancia", distancia)
@@ -53,11 +67,14 @@ object RouteService {
                 it.write(jsonInputString.toByteArray(Charsets.UTF_8))
             }
 
+            // Se obtiene la respuesta del servidor
             val responseCode = connection.responseCode
             val responseMessage = connection.inputStream.bufferedReader().use { it.readText() }
 
+            // Se cierra la conexión después de obtener la respuesta
             connection.disconnect()
 
+            // Si la respuesta es OK, regresa true, si no, regresa false
             if (responseCode == HttpURLConnection.HTTP_OK) {
                 Log.d("sendRoute", "Respuesta del servidor: $responseMessage")
                 true

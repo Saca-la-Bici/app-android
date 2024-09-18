c23edc040e79cea0e9fec972723bf20e69c1037f
alexmushi <A01705550@tec.mx>
2024-09-18 16:32:12 -0600

    Docs (Validación de input): Agregar comentarios

--- app/src/main/java/com/kotlin/sacalabici/utils/InputValidator.kt
+++ app/src/main/java/com/kotlin/sacalabici/utils/InputValidator.kt
@@ -1,14 +1,21 @@
 package com.kotlin.sacalabici.utils
 
+// Importaciones para TextWatcher y Editable
 import android.text.Editable
 import android.text.TextWatcher
 
+// Esta clase toma una función que se invoca cada vez que el texto cambia
 class InputValidator(private val callback: () -> Unit) : TextWatcher {
+
+    // Este método se llama antes de que el texto cambie
     override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
+    // Se llama mientras el texto está cambiando
+    // En este caso, se notifica que el texto ha cambiado
     override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-        callback.invoke()
+        callback.invoke()  // Se llama al callback
     }
 
+    // Después de que el texto cambie
     override fun afterTextChanged(s: Editable?) {}
 }

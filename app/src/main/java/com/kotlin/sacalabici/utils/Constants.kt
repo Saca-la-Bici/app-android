2acb6259b2c2844145af1e708d8493cbae032bbe
DanielQueijeiro <dany.queijeiro@hotmail.com>
2024-09-18 17:48:21 -0600

    <feat> Intercambiar orden del recyclerview para que se vea en el orden correcto

--- app/src/main/java/com/kotlin/sacalabici/utils/Constants.kt
+++ app/src/main/java/com/kotlin/sacalabici/utils/Constants.kt
@@ -1,5 +1,5 @@
 package com.kotlin.sacalabici.utils
 
 object Constants {
-    const val ANNOUNCEMENT_BASE_URL = "http://3.145.117.182:8080/"
+    const val ANNOUNCEMENT_BASE_URL = "http://3.145.117.182:7070/"
 }

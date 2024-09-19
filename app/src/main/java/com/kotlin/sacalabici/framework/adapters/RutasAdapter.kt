42f08daec9ef1ea708bf7c9842937d5fee1f48f0
A01710029 <palmadamartinez@gmail.com>
2024-09-19 00:32:08 -0600

    style (rutas): Cambiar bg de ruta seleccionada en lista

--- app/src/main/java/com/kotlin/sacalabici/framework/adapters/RutasAdapter.kt
+++ app/src/main/java/com/kotlin/sacalabici/framework/adapters/RutasAdapter.kt
@@ -2,14 +2,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.kotlin.sacalabici.R
 import com.kotlin.sacalabici.data.models.RutasBase
+import kotlinx.coroutines.selects.select
 
 class RutasAdapter(
     private var rutasList: List<RutasBase>,
     private val onRutaSelected: (RutasBase) -> Unit // Add the callback as a second parameter
 ) : RecyclerView.Adapter<RutasAdapter.RutasViewHolder>() {
+
+    // Para representar elemento seleccionado
+    private var selectedPosition: Int = RecyclerView.NO_POSITION
 
     class RutasViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val tituloTextView: TextView = itemView.findViewById(R.id.TVTitulo)
@@ -17,6 +22,7 @@ class RutasAdapter(
         val tiempoTextView: TextView = itemView.findViewById(R.id.TVTiempo)
         val nivelTextView: TextView = itemView.findViewById(R.id.TVNivel)
         val divider: View = itemView.findViewById(R.id.LLRutasDivider)
+        val rutaContainer: View = itemView.findViewById(R.id.rutaContainer)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RutasViewHolder {
@@ -32,13 +38,32 @@ class RutasAdapter(
         holder.tiempoTextView.text = "Tiempo Promedio: ${ruta.tiempo}"
         holder.nivelTextView.text = "Nivel: ${ruta.nivel}"
 
+        // Cambiar color de background de elemento seleccionado
+        if (selectedPosition == position) {
+            holder.rutaContainer.setBackgroundColor(
+                ContextCompat.getColor(holder.itemView.context, R.color.selected_bg)
+            )
+        } else {
+            holder.rutaContainer.setBackgroundColor(
+                ContextCompat.getColor(holder.itemView.context, R.color.white)
+            )
+        }
+
+        // Manejar cambio de elemento seleccionado via input de usuario
+        holder.itemView.setOnClickListener {
+            val previousPosition = selectedPosition
+            selectedPosition = position
+
+            // Actualizar posición de elemento seleccionado
+            notifyItemChanged(previousPosition)
+            notifyItemChanged(selectedPosition)
+
+            onRutaSelected(ruta)
+        }
+
         // Desactivar línea divisora para el último elemento
         holder.divider.visibility = if (position == rutasList.size - 1) View.GONE else View.VISIBLE
 
-        // Set click listener for the item
-        holder.itemView.setOnClickListener {
-            onRutaSelected(ruta) // Trigger the callback with the selected route
-        }
     }
 
     override fun getItemCount(): Int {

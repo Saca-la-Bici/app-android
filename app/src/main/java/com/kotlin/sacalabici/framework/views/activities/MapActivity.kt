1d4e7cfed49e1f9d7e67f8b9eabf93bd0c8e7603
Zathiald <A01705403@tec.mx>
2024-09-23 11:48:37 -0600

    Feat: Agregar mensaje de error si falla server

--- app/src/main/java/com/kotlin/sacalabici/framework/views/activities/MapActivity.kt
+++ app/src/main/java/com/kotlin/sacalabici/framework/views/activities/MapActivity.kt
@@ -4,9 +4,9 @@ import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.util.Log
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
 import androidx.fragment.app.Fragment
-import com.google.gson.JsonObject
 import com.kotlin.sacalabici.BuildConfig
 import com.kotlin.sacalabici.R
 import com.kotlin.sacalabici.data.models.CoordenadasBase
@@ -103,20 +103,27 @@ class MapActivity : BaseActivity(), RutasFragment.OnRutaSelectedListener {
             rutasFragmentVisible = false
         } else {
             lifecycleScope.launch {
-                val rutasList = RutasService.getRutasList()
-                if (rutasList != null) {
-                    val rutasFragment = RutasFragment.newInstance(rutasList, lastSelectedRuta)
-                    fragmentManager.beginTransaction()
-                        .replace(R.id.nav_host_fragment_content_main, rutasFragment)
-                        .addToBackStack(null)
-                        .commit()
-                    rutasFragmentVisible = true
-                } else {
-                    Log.e("MapActivity", "Error al obtener la lista de rutas")
+                try {
+                    val rutasList = RutasService.getRutasList()
+                    if (rutasList != null) {
+                        val rutasFragment = RutasFragment.newInstance(rutasList, lastSelectedRuta)
+                        fragmentManager.beginTransaction()
+                            .replace(R.id.nav_host_fragment_content_main, rutasFragment)
+                            .addToBackStack(null)
+                            .commit()
+                        rutasFragmentVisible = true
+                    } else {
+                        // Mensaje de error si la lista es null
+                        Toast.makeText(this@MapActivity, "Error al obtener la lista de rutas.", Toast.LENGTH_SHORT).show()
+                    }
+                } catch (e: Exception) {
+                    // Muestra un Toast con el mensaje de error si ocurre una excepción
+                    Toast.makeText(this@MapActivity, "Error al obtener la lista de rutas: ${e.message}", Toast.LENGTH_SHORT).show()
                 }
             }
         }
     }
+
 
     override fun onRutaSelected(ruta: RutasBase) {
         lastSelectedRuta = ruta

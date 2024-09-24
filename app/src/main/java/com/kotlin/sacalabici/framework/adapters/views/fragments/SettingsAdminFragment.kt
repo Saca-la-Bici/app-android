ec9a28ee5d614533a1b1d7c02c815d588f2f9231
Tony <111481792+Bowist27@users.noreply.github.com>
2024-09-24 16:17:17 -0600

    Feat: Cerrar Sesión

--- app/src/main/java/com/kotlin/sacalabici/framework/adapters/views/fragments/SettingsAdminFragment.kt
+++ app/src/main/java/com/kotlin/sacalabici/framework/adapters/views/fragments/SettingsAdminFragment.kt
@@ -1,12 +1,17 @@
 package com.kotlin.sacalabici.framework.adapters.views.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 import com.kotlin.sacalabici.R
 import com.kotlin.sacalabici.databinding.FragmentSettingsAdminBinding
+import com.kotlin.sacalabici.framework.adapters.views.activities.Session.SessionActivity
 
 
 class SettingsAdminFragment : Fragment() {
@@ -35,6 +40,23 @@ class SettingsAdminFragment : Fragment() {
                 .addToBackStack(null)
                 .commit()
         }
+        binding.BLogOut.setOnClickListener {
+            // Sign out the user from Firebase
+            Firebase.auth.signOut()
+
+            // Check if the user is signed out successfully
+            if (Firebase.auth.currentUser == null) {
+                // User is signed out, navigate to activity_session
+                val intent = Intent(activity, SessionActivity::class.java) // Replace with the correct class name for your activity
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK) // Clear the back stack
+                startActivity(intent)
+                activity?.finish() // Finish the current activity if necessary
+            } else {
+                // Handle the case where sign-out failed (optional)
+                Toast.makeText(context, "Sign out failed", Toast.LENGTH_SHORT).show()
+            }
+        }
+
 
         return binding.root
     }

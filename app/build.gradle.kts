37b4af0f1f31f9a3d22777f40e0246ed1b00540a
Zathiald <A01705403@tec.mx>
2024-09-26 09:22:37 -0600

    Refactor: Mover funciones principales a view model

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,8 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     //Glide
     implementation ("com.github.bumptech.glide:glide:4.12.0")
+    implementation(libs.play.services.fido)
+    implementation(libs.androidx.espresso.core)
     //implementation(libs.androidx.swiperefreshlayout)
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
     //Data binding

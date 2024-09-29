2effcaa143d80983afcdff17d164a887f64079af
Ángel Francisco García Guzmán <angelgarciag14@gmail.com>
2024-09-29 14:00:58 -0600

    Feat: Boton para visualizar la ruta en la actividad

--- app/src/main/java/com/kotlin/sacalabici/framework/views/activities/ActivityDetailActivity.kt
+++ app/src/main/java/com/kotlin/sacalabici/framework/views/activities/ActivityDetailActivity.kt
@@ -10,27 +10,19 @@ import androidx.core.app.ActivityCompat
 import com.kotlin.sacalabici.R
 import com.mapbox.maps.MapView
 import com.mapbox.maps.Style
-import com.mapbox.android.core.permissions.PermissionsManager
 import com.mapbox.maps.CameraOptions
 import com.mapbox.geojson.Point
 import okhttp3.*
 import org.json.JSONObject
-import java.io.IOException
-import okhttp3.MediaType.Companion.toMediaType
-import com.google.android.gms.tasks.Task
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import okhttp3.MediaType.Companion.toMediaType
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.RequestBody
-import okhttp3.Response
+import com.google.android.gms.tasks.Task
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.FusedLocationProviderClient
-import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import android.util.Log
-
+import android.view.View
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
 
 
 
@@ -38,12 +30,12 @@ class DetailActivity : AppCompatActivity() {
 
     private lateinit var mapView: MapView
     private lateinit var startButton: Button
+    private lateinit var ocultarButton: Button
 
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private val client = OkHttpClient()
 
     private val LOCATION_PERMISSION_REQUEST_CODE = 1000
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,29 +48,43 @@ class DetailActivity : AppCompatActivity() {
 
         // Inicializa el MapView
         mapView = findViewById(R.id.mapView)
+        val btnRuta: Button = findViewById(R.id.btnRuta)
+        ocultarButton = findViewById(R.id.btnOcultarRuta) // Inicializa el botón para ocultar el mapa
 
-        // Establece el estilo del mapa
-        mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS)
+        // Inicialmente ocultar el MapView
+        mapView.visibility = View.GONE
 
-        // Configura la cámara para centrarse en Querétaro
-        val queretaroLocation = CameraOptions.Builder()
-            .center(Point.fromLngLat(-100.3899, 20.5884)) // Longitud, Latitud
-            .zoom(12.0)
-            .build()
+        // Configurar el botón
+        btnRuta.setOnClickListener {
+            // Mostrar el MapView con el botón para quitarlo cuando se presione el botón "Ver Ruta"
+            mapView.visibility = View.VISIBLE
+            ocultarButton.visibility = View.VISIBLE
 
-        mapView.getMapboxMap().setCamera(queretaroLocation)
+            // Establece el estilo del mapa
+            mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS)
 
+            // Configura la cámara para centrarse en Querétaro
+            val queretaroLocation = CameraOptions.Builder()
+                .center(Point.fromLngLat(-100.3899, 20.5884)) // Longitud, Latitud
+                .zoom(12.0)
+                .build()
 
+            mapView.getMapboxMap().setCamera(queretaroLocation)
+        }
 
+        // Configurar el botón para ocultar el mapa
+        ocultarButton.setOnClickListener {
+            mapView.visibility = View.GONE
+            ocultarButton.visibility = View.GONE
+        }
 
-        // Configura el botón de iniciar
-        startButton = findViewById(R.id.btnStart) // Asegúrate de que el ID del botón sea correcto
-        startButton.setOnClickListener {
+        startButton = findViewById(R.id.btnStart)
+        startButton.setOnClickListener{
             obtenerUbicacionActual()
         }
     }
 
-    private fun obtenerUbicacionActual() {
+    fun obtenerUbicacionActual() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // Solicitar permisos de ubicación
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
@@ -130,6 +136,4 @@ class DetailActivity : AppCompatActivity() {
             }
         }
     }
-
-
 }

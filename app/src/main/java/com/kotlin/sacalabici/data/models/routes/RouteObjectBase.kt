9c85ab581ec2a71d60b371c64aefd85b1e3b18fd
Zathiald <A01705403@tec.mx>
2024-09-30 10:56:52 -0600

    Feat: Agregar RBAC a rutas

--- app/src/main/java/com/kotlin/sacalabici/data/models/routes/RouteObjectBase.kt
+++ app/src/main/java/com/kotlin/sacalabici/data/models/routes/RouteObjectBase.kt
@@ -1,10 +1,8 @@
 package com.kotlin.sacalabici.data.models.routes
 
-import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
-import kotlinx.android.parcel.Parcelize
 
 data class RouteObjectBase(
     @SerializedName("rutas") val routes: List<RouteBase>,
-    @SerializedName("permisos") val role: List<String>
+    @SerializedName("permisos") val permission: List<String>
 )

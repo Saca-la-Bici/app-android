26e8b4473334366fc8b59f18e9e3a09f03a4941a
PAOLA MARIA garrido <you@example.com>
2024-10-02 09:51:31 -0600

    feat: Agregar list requirement de consultar medallas

--- app/src/main/java/com/kotlin/sacalabici/domain/medals/ConsultMedalsListRequirement.kt
+++ app/src/main/java/com/kotlin/sacalabici/domain/medals/ConsultMedalsListRequirement.kt
@@ -1,6 +1,5 @@
 package com.kotlin.sacalabici.domain.medals
 
-import com.kotlin.sacalabici.data.models.medals.MedalBase
 import com.kotlin.sacalabici.data.models.medals.MedalObjectBase
 import com.kotlin.sacalabici.data.repositories.medals.MedalsRepository
 

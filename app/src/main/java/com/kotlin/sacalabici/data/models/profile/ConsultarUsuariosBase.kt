fa82be711846534d7ef63567ecf12d1e888a1a2d
A01710217 <a01710217@tec.mx>
2024-10-01 19:16:31 -0600

    fix(): poner imagen

--- app/src/main/java/com/kotlin/sacalabici/data/models/profile/ConsultarUsuariosBase.kt
+++ app/src/main/java/com/kotlin/sacalabici/data/models/profile/ConsultarUsuariosBase.kt
@@ -6,5 +6,6 @@ import com.kotlin.sacalabici.data.models.user.Rol
 
 data class ConsultarUsuariosBase(
     @SerializedName("usuario") val usuario: BriefUser,
-    @SerializedName("rol") val rol: Rol
+    @SerializedName("rol") val rol: Rol,
+    @SerializedName("imagenPerfil") val imagenPerfil: String
 )
--- app/src/main/java/com/kotlin/sacalabici/framework/viewholders/ConsultarUsuariosViewHolder.kt
+++ app/src/main/java/com/kotlin/sacalabici/framework/viewholders/ConsultarUsuariosViewHolder.kt
@@ -1,6 +1,8 @@
 package com.kotlin.sacalabici.framework.adapters.viewhoiders
 
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.kotlin.sacalabici.R
 import com.kotlin.sacalabici.data.models.profile.ConsultarUsuariosBase
 import com.kotlin.sacalabici.databinding.ItemUserBinding
@@ -17,9 +19,25 @@ class ConsultarUsuariosViewHolder(val binding: ItemUserBinding) : RecyclerView.V
         } else if (item.rol.nombreRol == "Staff") {
             binding.btnChangerol.text = "Eliminar"
             binding.btnChangerol.setBackgroundColor(itemView.context.getColor(R.color.lightGray))
-        }else if (item.rol.nombreRol == "Usuario") {
+        } else if (item.rol.nombreRol == "Usuario") {
             binding.btnChangerol.text = "Agregar"
             binding.btnChangerol.setBackgroundColor(itemView.context.getColor(R.color.yellow))
+        }
+
+        // Cargar imagen de perfil usando Glide
+        val profileImageUrl = item.imagenPerfil
+
+        // Si la URL de la imagen no es nula ni vacía, cargarla con Glide
+        if (!profileImageUrl.isNullOrEmpty()) {
+            Glide.with(itemView.context)
+                .load(profileImageUrl) // Cargar la imagen desde la URL
+                .diskCacheStrategy(DiskCacheStrategy.ALL) // Cachear la imagen
+                .placeholder(R.drawable.baseline_person_24) // Imagen por defecto mientras se carga
+                .error(R.drawable.baseline_person_24) // Imagen por defecto en caso de error
+                .into(binding.RVuserImage) // Colocar la imagen en el ImageView
+        } else {
+            // Si la URL es nula o vacía, usar la imagen por defecto
+            binding.RVuserImage.setImageResource(R.drawable.baseline_person_24)
         }
     }
 }

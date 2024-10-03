3e09b864db7f61539e2a71967999ca5a23e78871
PAOLA MARIA garrido <you@example.com>
2024-10-03 07:58:47 -0600

    feat: Agregar Repo con log test

--- app/src/main/java/com/kotlin/sacalabici/data/repositories/medals/MedalsRepository.kt
+++ app/src/main/java/com/kotlin/sacalabici/data/repositories/medals/MedalsRepository.kt
@@ -1,16 +1,24 @@
 package com.kotlin.sacalabici.data.repositories.medals
 
+import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
 import com.kotlin.sacalabici.data.models.medals.MedalObjectBase
 import com.kotlin.sacalabici.data.network.FirebaseTokenManager
 import com.kotlin.sacalabici.data.network.medals.MedalsApiClient
 
 class MedalsRepository {
-
     val firebaseAuth = FirebaseAuth.getInstance()
     val firebaseTokenManager = FirebaseTokenManager(firebaseAuth)
     private val apiMedals = MedalsApiClient(firebaseTokenManager)
 
     suspend fun getMedalsList(): MedalObjectBase? =
-        apiMedals.getMedalsList()
+        try{
+            // Realizar la consulta
+            val response = apiMedals.getMedalsList()
+            Log.d("MedalsRepository", "Consulta exitosa: $response")
+            response
+        } catch (e: Exception){
+            Log.e("Falla en getMedalsList", "Error en la consulta de getMedalsList: ${e.message}")
+            null
+        }
 }

be3fca022337658b4754962cdabf8ead20576d1e
Diego <a01705506@tec.mx>
2024-10-06 14:55:25 -0600

    feat(modificar perfil) - Modificar imágenes

--- app/src/main/java/com/kotlin/sacalabici/framework/views/fragments/ProfileEditFragment.kt
+++ app/src/main/java/com/kotlin/sacalabici/framework/views/fragments/ProfileEditFragment.kt
@@ -1,27 +1,36 @@
 package com.kotlin.sacalabici.framework.views.fragments
 
+import android.app.Activity
 import android.app.Activity.RESULT_OK
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import com.kotlin.sacalabici.R
-import com.kotlin.sacalabici.databinding.FragmentProfileEditBinding
-import com.kotlin.sacalabici.framework.viewmodel.ProfileViewModel
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.kotlin.sacalabici.R
 import com.kotlin.sacalabici.data.models.profile.Profile
+import com.kotlin.sacalabici.databinding.FragmentProfileEditBinding
+import com.kotlin.sacalabici.framework.viewmodel.ProfileViewModel
 
 
 class ProfileEditFragment: Fragment() {
+
     private var _binding: FragmentProfileEditBinding? = null
     private val binding get() = _binding!!
     private lateinit var editProfileLauncher: ActivityResultLauncher<Intent>
     private lateinit var viewModel: ProfileViewModel
+    private lateinit var pickImageLauncher: ActivityResultLauncher<Intent>
+    private var selectedImageUri: Uri? = null
+
     override fun onCreateView(
 
         inflater: LayoutInflater,
@@ -37,6 +46,8 @@ class ProfileEditFragment: Fragment() {
         setupBloodDropdown()
         setupBackButton()
         setupUploadButton()
+        setUpEditImageButton()
+        registerImagePicker()
 
         editProfileLauncher=registerForActivityResult(
             ActivityResultContracts.StartActivityForResult()
@@ -61,6 +72,20 @@ class ProfileEditFragment: Fragment() {
 //                binding.genderDropDown.setText(profile.activitiesCompleted, false)
                 binding.bloodDropDown.setText(profile.bloodtype, false)
                 binding.emergencyNumber.setText(profile.emergencyNumber)
+                val profileImageUrl = profile.pImage
+
+                if (!profileImageUrl.isNullOrEmpty()) {
+                    Glide
+                        .with(requireContext())
+                        .load(profileImageUrl) // Cargar la imagen desde la URL
+                        .diskCacheStrategy(DiskCacheStrategy.ALL) // Cachear la imagen
+                        .placeholder(R.drawable.baseline_person_24) // Imagen por defecto mientras se carga
+                        .error(R.drawable.baseline_person_24) // Imagen por defecto en caso de error
+                        .into(binding.profileImage) // Colocar la imagen en el ImageView
+                } else {
+                    // Si la URL es nula o vacía, usar la imagen por defecto
+                    binding.profileImage.setImageResource(R.drawable.baseline_person_24)
+                }
             }
         }
     }
@@ -108,7 +133,9 @@ class ProfileEditFragment: Fragment() {
     }
 
     private fun setupUploadButton() {
-        binding.btnSave.setOnClickListener {
+        val saveButton = binding.btnSave
+        saveButton.setOnClickListener {
+            val image = selectedImageUri
             val name = binding.name.text.toString()
             val username = binding.username.text.toString()
 //            val gender = binding.genderDropDown.text.toString()
@@ -122,6 +149,30 @@ class ProfileEditFragment: Fragment() {
                 .replace(R.id.nav_host_fragment_content_main, profileFragment)
                 .addToBackStack(null)
                 .commit()
+        }
+    }
+
+    private fun setUpEditImageButton(){
+        val imageButton = binding.profileImageLayout
+        imageButton.setOnClickListener{
+//            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+////            pickImageLauncher.launch(intent)
+//            val i = Intent(
+//                Intent.ACTION_PICK,
+//                MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                pickImageLauncher.launch(intent)
+
+//            startActivityForResult(i, IMAGE_PICKER_SELECT)
+        }
+    }
+
+    private fun registerImagePicker() {
+        pickImageLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                selectedImageUri = result.data?.data
+                binding.profileImage.setImageURI(selectedImageUri)
+            }
         }
     }
 

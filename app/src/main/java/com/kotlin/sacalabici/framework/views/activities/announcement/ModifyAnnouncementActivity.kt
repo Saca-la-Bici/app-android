ea30f3de368a867fb2b0ac65ee948fe312f635e3
DanielContrerasCh <danicontreras0409@gmail.com>
2024-10-06 02:16:16 -0600

    <fix>: No eliminar anterior imagen cuando se modifica el anuncio

--- app/src/main/java/com/kotlin/sacalabici/framework/views/activities/announcement/ModifyAnnouncementActivity.kt
+++ app/src/main/java/com/kotlin/sacalabici/framework/views/activities/announcement/ModifyAnnouncementActivity.kt
@@ -36,7 +36,7 @@ class ModifyAnnouncementActivity: AppCompatActivity() {
 
 
         viewModel = ViewModelProvider(this)[AnnouncementsViewModel::class.java]
-        initializeListeners()
+        initializeListeners(url)
         registerImagePicker()
     }
 
@@ -50,7 +50,7 @@ class ModifyAnnouncementActivity: AppCompatActivity() {
         }
     }
 
-    private fun initializeListeners() {
+    private fun initializeListeners(url: String?) {
         binding.ibClose.setOnClickListener {
             finish()
         }
@@ -60,8 +60,10 @@ class ModifyAnnouncementActivity: AppCompatActivity() {
             val title = binding.etModifyAnnouncementTitle.text.toString()
             val description = binding.etModifyAnnouncementDescription.text.toString()
             val image = selectedImageUri
+            Log.d("image", "url: $url")
+            Log.d("image", "selectedimage: $selectedImageUri")
             val announcement = Announcement(title, description, image)
-            viewModel.patchAnnouncement(id, announcement)
+            viewModel.patchAnnouncement(id, announcement, this)
             setResult(Activity.RESULT_OK)
             finish()
         }

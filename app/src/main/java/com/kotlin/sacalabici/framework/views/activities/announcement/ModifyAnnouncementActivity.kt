299a344e61ffd143602002b0a9d561a9c22a80c0
DanielContrerasCh <danicontreras0409@gmail.com>
2024-10-06 13:38:47 -0600

    <style>: Quitar logs

--- app/src/main/java/com/kotlin/sacalabici/framework/views/activities/announcement/ModifyAnnouncementActivity.kt
+++ app/src/main/java/com/kotlin/sacalabici/framework/views/activities/announcement/ModifyAnnouncementActivity.kt
@@ -60,8 +60,6 @@ class ModifyAnnouncementActivity: AppCompatActivity() {
             val title = binding.etModifyAnnouncementTitle.text.toString()
             val description = binding.etModifyAnnouncementDescription.text.toString()
             val image = selectedImageUri
-            Log.d("image", "url: $url")
-            Log.d("image", "selectedimage: $selectedImageUri")
             val announcement = Announcement(title, description, image)
             viewModel.patchAnnouncement(id, announcement, this)
             setResult(Activity.RESULT_OK)

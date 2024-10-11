113539ac9219487afd920532fd83839c7004e8c1
PAOLA MARIA garrido <you@example.com>
2024-10-10 18:50:17 -0600

    feat: Ajustar modelos de medallas

--- app/src/main/java/com/kotlin/sacalabici/data/models/medals/MedalObjectBase.kt
+++ app/src/main/java/com/kotlin/sacalabici/data/models/medals/MedalObjectBase.kt
@@ -3,8 +3,5 @@ package com.kotlin.sacalabici.data.models.medals
 import com.google.gson.annotations.SerializedName
 
 data class MedalObjectBase(
-    @SerializedName("code") val code: Int?,
-    @SerializedName("msg") val message: String?,
-    @SerializedName("permisos") val permissions: ArrayList<String>,
-    @SerializedName("data") val medals: ArrayList<MedalBase>
+    @SerializedName("medallasActivas") val medals: ArrayList<MedalBase>
 )

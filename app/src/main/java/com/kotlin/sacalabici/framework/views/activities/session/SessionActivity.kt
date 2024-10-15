6c2f0b4f7bb6f15e04471a2266e25afe2c99a0ae
Tony <111481792+Bowist27@users.noreply.github.com>
2024-10-15 16:40:18 -0600

    feat (Terms&Conditions,Policies)

--- app/src/main/java/com/kotlin/sacalabici/framework/views/activities/session/SessionActivity.kt
+++ app/src/main/java/com/kotlin/sacalabici/framework/views/activities/session/SessionActivity.kt
@@ -2,14 +2,18 @@
 package com.kotlin.sacalabici.framework.views.activities.session
 
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.material.imageview.ShapeableImageView
 import com.google.firebase.auth.FirebaseAuth
+import com.kotlin.sacalabici.R
 import com.kotlin.sacalabici.data.models.session.AuthState
 import com.kotlin.sacalabici.databinding.ActivitySessionBinding
 import com.kotlin.sacalabici.framework.viewmodel.session.AuthViewModel
@@ -24,7 +28,7 @@ class SessionActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         initializeBinding()
 
-        // Inicializa el ViewModel con Firebase y GoogleSignInOptions
+        // Initialize ViewModel with Firebase and GoogleSignInOptions
         authViewModel.initialize(
             FirebaseAuth.getInstance(),
             GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -38,11 +42,10 @@ class SessionActivity : AppCompatActivity() {
         authViewModel.authState.observe(this) { authState ->
             when (authState) {
                 is AuthState.Success -> {
-                    // Registration successful
                     val intent = Intent(this, MainActivity::class.java)
                     intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
                     startActivity(intent)
-                    finish() // Optional: Finish RegisteerContinueActivity to prevent going back
+                    finish()
                 }
                 is AuthState.Error -> {
                     Toast.makeText(this, authState.message, Toast.LENGTH_SHORT).show()
@@ -67,15 +70,40 @@ class SessionActivity : AppCompatActivity() {
             }
         }
 
-        // Listener para el TextView de política de privacidad
+        // Setup WebView and close button (ShapeableImageView)
+        val webView: WebView = findViewById(R.id.webView)
+        val closeWebView: ShapeableImageView = findViewById(R.id.closeWebView)  // Cast as ShapeableImageView
+
+        // Configure WebView to open links within the app
+        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true // Enable JavaScript if necessary
+        webView.settings.domStorageEnabled = true
+        webView.settings.allowFileAccess = true
+
+
+        // Listener for Privacy Policy link
         binding.TVPrivacyPolicy.setOnClickListener {
             val url = "http://18.220.205.53:8080/politicasAplicacion/politicaPrivacidad"
-            val intent = Intent(Intent.ACTION_VIEW)
-            intent.data = Uri.parse(url)
-            startActivity(intent)
+            webView.loadUrl(url)
+            webView.visibility = View.VISIBLE
+            closeWebView.visibility = View.VISIBLE
         }
 
-        // Listeners para los botones
+        // Listener for Privacy Policy link
+        binding.TVTermsAndConditions.setOnClickListener {
+            val url = "http://18.220.205.53:8080/politicasAplicacion/terminosCondiciones"
+            webView.loadUrl(url)
+            webView.visibility = View.VISIBLE
+            closeWebView.visibility = View.VISIBLE
+        }
+
+        // Listener for closing the WebView
+        closeWebView.setOnClickListener {
+            webView.visibility = View.GONE
+            closeWebView.visibility = View.GONE
+        }
+
+        // Listeners for login buttons
         binding.BGoogle.setOnClickListener {
             authViewModel.signInWithGoogle(this)
         }
